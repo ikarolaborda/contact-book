7bbbb415bf894fa472cd1b33b3940b441de346af
Ikaro C. Laborda <iclaborda@msn.com>
2022-11-20 00:59:48 +0000

    feat: setting up initial structure for dealing with vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
 require('./bootstrap');
 
-import Alpine from 'alpinejs';
+import { createApp } from 'vue';
+import ContactsIndex from './components/Contacts/Index';
 
-window.Alpine = Alpine;
-
-Alpine.start();
+const app = createApp({});
+app.component('contacts-index', ContactsIndex);
+app.mount('#app');
