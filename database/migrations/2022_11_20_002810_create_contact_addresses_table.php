ed1ac63dc60fe9acc5faa2206246a95456935cd1
Ikaro C. Laborda <iclaborda@msn.com>
2022-11-20 00:30:46 +0000

    feat: setting up basic structure for contact addresses entity

---
+++ database/migrations/2022_11_20_002810_create_contact_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contact_id')->constrained('contact');
+            $table->string('state');
+            $table->string('city');
+            $table->string('neighborhood');
+            $table->string('address');
+            $table->string('number');
+            $table->string('complement')->nullable();
+            $table->string('zip_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_addresses');
+    }
+};
